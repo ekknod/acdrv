b55f16c6225147d8410648ed33bf6f1b75a52f02
ekknod <ekknod@gmail.com>
2022-07-28 12:57:56 +0300

    MouseClassServiceCallback hook is added

--- main.c
+++ main.c
@@ -48,6 +48,14 @@ PVOID thread_object;
 HANDLE thread_handle;
 BOOLEAN gExitCalled;
 
+QWORD _KeAcquireSpinLockAtDpcLevel;
+QWORD _KeReleaseSpinLockFromDpcLevel;
+QWORD _IofCompleteRequest;
+QWORD _IoReleaseRemoveLockEx;
+
+BOOL mouse_hook(void);
+void mouse_unhook(void);
+
 __declspec(dllimport)
 BOOLEAN PsGetProcessExitProcessCalled(PEPROCESS process);
 
@@ -506,6 +514,8 @@ DriverUnload(
 	(DriverObject);
 	gExitCalled = 1;
 
+	mouse_unhook();
+
 	if (thread_object) {
 		KeWaitForSingleObject(
 			(PVOID)thread_object,
@@ -519,6 +529,10 @@ DriverUnload(
 
 		ZwClose(thread_handle);
 	}
+
+
+	// Lets wait second, everything should be unloaded now.
+	NtSleep(1000);
 
 	DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "[+] Anti-Cheat.sys is closed\n");
 }
@@ -626,6 +640,9 @@ NTSTATUS DriverEntry(
 	DriverObject->DriverUnload = DriverUnload;
 
 
+	mouse_hook();
+
+
 	CLIENT_ID thread_id;
 	PsCreateSystemThread(&thread_handle, STANDARD_RIGHTS_ALL, NULL, NULL, &thread_id, (PKSTART_ROUTINE)system_thread, (PVOID)0);
 	ObReferenceObjectByHandle(
@@ -668,3 +685,257 @@ BOOL IsInValidRange(QWORD address)
 	
 	return 0;
 }
+
+
+#pragma warning(disable : 4201)
+typedef struct _MOUSE_INPUT_DATA {
+	USHORT UnitId;
+	USHORT Flags;
+	union {
+	ULONG Buttons;
+	struct {
+	USHORT ButtonFlags;
+	USHORT ButtonData;
+	};
+	};
+	ULONG  RawButtons;
+	LONG   LastX;
+	LONG   LastY;
+	ULONG  ExtraInformation;
+} MOUSE_INPUT_DATA, *PMOUSE_INPUT_DATA;
+
+typedef VOID
+(*MouseClassServiceCallbackFn)(
+	PDEVICE_OBJECT DeviceObject,
+	PMOUSE_INPUT_DATA InputDataStart,
+	PMOUSE_INPUT_DATA InputDataEnd,
+	PULONG InputDataConsumed
+);
+
+typedef struct _MOUSE_OBJECT
+{
+	PDEVICE_OBJECT              mouse_device;
+	MouseClassServiceCallbackFn service_callback;
+	BOOL                        hook;
+	QWORD                       hid;
+	QWORD                       hid_length;
+} MOUSE_OBJECT, * PMOUSE_OBJECT;
+
+MOUSE_OBJECT gMouseObject;
+
+
+// https://github.com/btbd/umap/blob/master/mapper/util.c#L117
+BOOLEAN MemCopyWP(PVOID dest, PVOID src, ULONG length) {
+	PMDL mdl = IoAllocateMdl(dest, length, FALSE, FALSE, NULL);
+	if (!mdl) {
+		return FALSE;
+	}
+
+	MmProbeAndLockPages(mdl, KernelMode, IoModifyAccess);
+
+	PVOID mapped = MmMapLockedPagesSpecifyCache(mdl, KernelMode, MmNonCached, NULL, 0, HighPagePriority);
+	if (!mapped) {
+		MmUnlockPages(mdl);
+		IoFreeMdl(mdl);
+		return FALSE;
+	}
+
+	memcpy(mapped, src, length);
+
+	MmUnmapLockedPages(mapped, mdl);
+	MmUnlockPages(mdl);
+	IoFreeMdl(mdl);
+	return TRUE;
+}
+
+#define JMP_SIZE 14
+
+// https://github.com/btbd/umap/
+VOID *TrampolineHook(VOID *dest, VOID *src, UINT8 original[JMP_SIZE]) {
+	if (original) {
+		MemCopyWP(original, src, JMP_SIZE);
+	}
+
+	unsigned char bytes[] = "\xFF\x25\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00";
+	*(QWORD*)(bytes + 6) = (QWORD)dest;
+
+	MemCopyWP(src, bytes, JMP_SIZE);
+
+	return src;
+}
+
+VOID TrampolineUnHook(VOID *src, UINT8 original[JMP_SIZE]) {
+	MemCopyWP(src, original, JMP_SIZE);
+}
+
+
+
+QWORD MouseClassServiceCallback(
+	PDEVICE_OBJECT DeviceObject,
+	PMOUSE_INPUT_DATA InputDataStart,
+	PMOUSE_INPUT_DATA InputDataEnd,
+	PULONG InputDataConsumed
+);
+
+QWORD MouseClassServiceCallbackHook(
+	PDEVICE_OBJECT DeviceObject,
+	PMOUSE_INPUT_DATA InputDataStart,
+	PMOUSE_INPUT_DATA InputDataEnd,
+	PULONG InputDataConsumed
+)
+{
+	(DeviceObject);
+	(InputDataStart);
+	(InputDataEnd);
+	(InputDataConsumed);
+
+	QWORD address = (QWORD)_ReturnAddress();
+	if (address < (QWORD)gMouseObject.hid && address > (QWORD)((QWORD)gMouseObject.hid + gMouseObject.hid_length))
+	{
+		DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "Mouse manipulation was detected (%llx)\n", _ReturnAddress());
+	}
+
+	// C/C++ -> All Options -> Control Overflow Guard : OFF, otherwise compiler will create CALL instruction and it will BSOD.
+	// Sadly inline assembly is not supported by x64 C/C++.
+	return ((QWORD(*)(PDEVICE_OBJECT, PMOUSE_INPUT_DATA, PMOUSE_INPUT_DATA, PULONG))((QWORD)MouseClassServiceCallback + 5))(
+		DeviceObject,
+		InputDataStart,
+		InputDataEnd,
+		InputDataConsumed
+		);
+
+	
+}
+
+
+unsigned char OriginalMouseClassService[JMP_SIZE];
+
+NTSYSCALLAPI
+NTSTATUS
+ObReferenceObjectByName(
+      __in PUNICODE_STRING ObjectName,
+      __in ULONG Attributes,
+      __in_opt PACCESS_STATE AccessState,
+      __in_opt ACCESS_MASK DesiredAccess,
+      __in POBJECT_TYPE ObjectType,
+      __in KPROCESSOR_MODE AccessMode,
+      __inout_opt PVOID ParseContext,
+      __out PVOID *Object
+  );
+
+NTSYSCALLAPI
+POBJECT_TYPE* IoDriverObjectType;
+
+BOOL mouse_hook(void)
+{
+	
+	_KeAcquireSpinLockAtDpcLevel = (QWORD)KeAcquireSpinLockAtDpcLevel;
+	_KeReleaseSpinLockFromDpcLevel = (QWORD)KeReleaseSpinLockFromDpcLevel;
+	_IofCompleteRequest = (QWORD)IofCompleteRequest;
+	_IoReleaseRemoveLockEx = (QWORD)IoReleaseRemoveLockEx;
+
+	// https://github.com/nbqofficial/norsefire
+	if (gMouseObject.hook == 0) {
+
+		UNICODE_STRING class_string;
+		RtlInitUnicodeString(&class_string, L"\\Driver\\MouClass");
+	
+
+		PDRIVER_OBJECT class_driver_object = NULL;
+		NTSTATUS status = ObReferenceObjectByName(&class_string, OBJ_CASE_INSENSITIVE, NULL, 0, *IoDriverObjectType, KernelMode, NULL, (PVOID*)&class_driver_object);
+		if (!NT_SUCCESS(status)) {
+			return 0;
+		}
+
+		UNICODE_STRING hid_string;
+		RtlInitUnicodeString(&hid_string, L"\\Driver\\MouHID");
+	
+
+		PDRIVER_OBJECT hid_driver_object = NULL;
+	
+		status = ObReferenceObjectByName(&hid_string, OBJ_CASE_INSENSITIVE, NULL, 0, *IoDriverObjectType, KernelMode, NULL, (PVOID*)&hid_driver_object);
+		if (!NT_SUCCESS(status))
+		{
+			if (class_driver_object) {
+				ObfDereferenceObject(class_driver_object);
+			}
+			return 0;
+		}
+
+		gMouseObject.hid = (QWORD)hid_driver_object->DriverStart;
+		gMouseObject.hid_length = (QWORD)hid_driver_object->DriverSize;
+
+		PVOID class_driver_base = NULL;
+
+
+		PDEVICE_OBJECT hid_device_object = hid_driver_object->DeviceObject;
+		while (hid_device_object && !gMouseObject.service_callback)
+		{
+			PDEVICE_OBJECT class_device_object = class_driver_object->DeviceObject;
+			while (class_device_object && !gMouseObject.service_callback)
+			{
+				if (!class_device_object->NextDevice && !gMouseObject.mouse_device)
+				{
+					gMouseObject.mouse_device = class_device_object;
+				}
+
+				PULONG_PTR device_extension = (PULONG_PTR)hid_device_object->DeviceExtension;
+				ULONG_PTR device_ext_size = ((ULONG_PTR)hid_device_object->DeviceObjectExtension - (ULONG_PTR)hid_device_object->DeviceExtension) / 4;
+				class_driver_base = class_driver_object->DriverStart;
+				for (ULONG_PTR i = 0; i < device_ext_size; i++)
+				{
+					if (device_extension[i] == (ULONG_PTR)class_device_object && device_extension[i + 1] > (ULONG_PTR)class_driver_object)
+					{
+						gMouseObject.service_callback = (MouseClassServiceCallbackFn)(device_extension[i + 1]);
+					
+						break;
+					}
+				}
+				class_device_object = class_device_object->NextDevice;
+			}
+			hid_device_object = hid_device_object->AttachedDevice;
+		}
+	
+		if (!gMouseObject.mouse_device)
+		{
+			PDEVICE_OBJECT target_device_object = class_driver_object->DeviceObject;
+			while (target_device_object)
+			{
+				if (!target_device_object->NextDevice)
+				{
+					gMouseObject.mouse_device = target_device_object;
+					break;
+				}
+				target_device_object = target_device_object->NextDevice;
+			}
+		}
+
+		ObfDereferenceObject(class_driver_object);
+		ObfDereferenceObject(hid_driver_object);
+
+		if (gMouseObject.mouse_device && gMouseObject.service_callback) {	
+
+			TrampolineHook((void *)MouseClassServiceCallback, (void *)gMouseObject.service_callback,  OriginalMouseClassService);
+
+			gMouseObject.hook = 1;
+
+			return 1;
+
+		}
+	} else {
+		return 1;
+	}
+
+	return 0;
+
+	
+}
+
+void mouse_unhook(void)
+{
+	if (gMouseObject.hook) {
+		TrampolineUnHook((void *)gMouseObject.service_callback, OriginalMouseClassService);
+		gMouseObject.hook = 0;
+	}
+}
+
